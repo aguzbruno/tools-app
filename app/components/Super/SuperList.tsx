4ec70630008938acf224315fbc9544a65ab9c470
agustin-bruno-conexa <agustin.bruno@conexa.ai>
2024-07-29 17:50:42 -0300

    margin botom

--- app/components/Super/SuperList.tsx
+++ app/components/Super/SuperList.tsx
@@ -160,7 +160,7 @@ const SuperList = () => {
 
 
         </div>
-        <ul className="space-y-2 mb-5">
+        <ul className="space-y-2 mb-8">
         {Object.keys(restCategories).map((category) => {
            const productsInCategory = restCategories[category];
            const totalProducts = productsInCategory.length;
