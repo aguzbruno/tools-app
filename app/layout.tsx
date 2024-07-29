2c02cfb50b94c13adb67178ba802fd15297c6cb1
agustin-bruno-conexa <agustin.bruno@conexa.ai>
2024-07-29 13:49:26 -0300

    pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,22 +5,39 @@ import "./globals.css";
 import { Providers } from "./providers";
 import Header from "./components/Header";
 import { useDarkMode } from "./store/darkMode";
+import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ["latin"] });
-
-
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const {darkMode} = useDarkMode();
+  const { darkMode } = useDarkMode();
+
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        }).catch((err) => {
+          console.error('Service Worker registration failed:', err);
+        });
+      });
+    }
+  }, []);
+
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body className={`${darkMode ? 'bg-black' : 'bg-white'}`}>
         <Providers>
-          <Header/>{children}
+          <Header />
+          {children}
         </Providers>
       </body>
     </html>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config;
+  },
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+});
 
 export default nextConfig;
