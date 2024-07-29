058851b7a9ca97228672662c77d69a20a8e35a43
agustin-bruno-conexa <agustin.bruno@conexa.ai>
2024-07-29 13:52:09 -0300

    SERVICE WORKER

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
-import withPWA from 'next-pwa';
+import withPWA from 'next-pwa'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})({
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
@@ -14,13 +16,8 @@ const nextConfig = withPWA({
       ...config.experiments,
       topLevelAwait: true,
     }
-    return config;
-  },
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  },
-});
+    return config
+  }
+})
 
-export default nextConfig;
+export default nextConfig
