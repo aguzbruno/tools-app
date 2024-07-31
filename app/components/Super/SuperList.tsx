5c306fc0ed37cc1a866a3b4f17c3eddf46d9b43f
agustin-bruno-conexa <76462423+aguzbruno@users.noreply.github.com>
2024-07-31 09:10:29 -0300

    tools

--- app/components/Super/SuperList.tsx
+++ app/components/Super/SuperList.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSuperStore } from "../../store/superStore";
 import Link from "next/link";
 import {
@@ -7,13 +7,15 @@ import {
   getProducts,
   getShoppingList,
   removeFromShoppingList,
-  saveShoppingHistory
+  saveShoppingHistory,
 } from "../../services/superService";
-import { Product,ShoppingListProduct } from "../../services/types";
-import Bomb from '../../assets/bomb.svg'
+import { Product, ShoppingListProduct } from "../../services/types";
+import Bomb from "../../assets/bomb.svg";
 import Image from "next/image";
 import ProductCardDetail from "./ProductCard/ProductCardDetail";
-import Save from '../../assets/save.svg'
+import Save from "../../assets/save.svg";
+import toast, { Toaster } from 'react-hot-toast';
+
 const SuperList = () => {
   const products = useSuperStore((state) => state.products);
   const shoppingList = useSuperStore((state) => state.shoppingList);
@@ -22,14 +24,16 @@ const SuperList = () => {
 
   const handleSaveHistory = async () => {
     await saveShoppingHistory(shoppingList);
+    toast('Se ha guardado correctamente')
   };
   const handleClearShoppingList = async () => {
     try {
       await clearShoppingList();
       const updatedShoppingList = await getShoppingList();
       setShoppingList(updatedShoppingList);
+      toast('Se han eliminado todos los productos de la lista')
     } catch (error) {
-      console.error('Error clearing shopping list:', error);
+      console.error("Error clearing shopping list:", error);
     }
   };
   const fetchProducts = async () => {
@@ -50,10 +54,9 @@ const SuperList = () => {
     }
   };
 
- 
   const groupByCategory = (products: Product[]) => {
     return products.reduce((acc, product) => {
-      const category = product.category || 'Otros';
+      const category = product.category || "Otros";
       if (!acc[category]) {
         acc[category] = [];
       }
@@ -65,19 +68,19 @@ const SuperList = () => {
   const groupedShopProducts = groupByCategory(shoppingList);
   const { OtrosShopp, ...restCategoriesShopp } = groupedShopProducts;
   const { Otros, ...restCategories } = groupedProducts;
-   // Estado para manejar las categorías abiertas
-   const [openCategories, setOpenCategories] = useState<{ [key: string]: boolean }>({});
+  // Estado para manejar las categorías abiertas
+  const [openCategories, setOpenCategories] = useState<{
+    [key: string]: boolean;
+  }>({});
 
-   // Función para alternar la apertura/cierre de una categoría
-   const toggleCategory = (category: string) => {
-     setOpenCategories((prevState) => ({
-       ...prevState,
-       [category]: !prevState[category],
-     }));
-   };
+  // Función para alternar la apertura/cierre de una categoría
+  const toggleCategory = (category: string) => {
+    setOpenCategories((prevState) => ({
+      ...prevState,
+      [category]: !prevState[category],
+    }));
+  };
 
-
-  
   useEffect(() => {
     fetchProducts();
     fetchShoppingList();
@@ -85,133 +88,211 @@ const SuperList = () => {
 
   return (
     <div className="container mx-auto px-4 py-6">
-      {shoppingList.length === 0 ?
-  (<button type="button" className="bg-red-300 font-bold rounded-md p-5 flex flex-row text-white m-auto justify-center items-center ">
- <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-    </svg>
- Loading
-</button>): (      
-  <>
-      <div className="mb-6">
-        <div className="flex flex-row justify-between items-center mb-4">
-          <div className="flex flex-col">
-        <p className="text-xs font-bold text-gray-300 " style={{color:"C4C4C4"}}>
-         LISTA DEL SUPER
-        </p>
-        <div className="flex items-center gap-2">
-        <p className="text-md font-bold text-gray-500 " style={{color:"C4C4C4"}}>
-        ESTIMADO :</p> €{shoppingList.reduce((acc, product) => acc + (product.price || 0) * (product.amount ? Number(product.amount) : 1), 0).toFixed(2)}
-
-         </div>
-        </div>
-        <div className="flex gap-3">
-        <Image
-             onClick={() => handleSaveHistory()}
-            className="cursor-pointer"
-            height={20}
-            width={20}
-            src={Save}
-            alt="Guardar"
-          />
-          <Image onClick={handleClearShoppingList} className="cursor-pointer" src={Bomb} alt="vaciar" width={20} height={20} />
-          </div>
-          </div>
+      <Toaster/>
+      {!shoppingList ? (
+        <button
+          type="button"
+          className="bg-red-300 font-bold rounded-md p-5 flex flex-row text-white m-auto justify-center items-center "
+        >
+          <svg
+            className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          Loading
+        </button>
+      ) : (
         
-        <ul className="space-y-2">
-        {Object.keys(restCategoriesShopp).map((category) => {
-           const productsInCategory = restCategoriesShopp[category];
-           const totalProducts = productsInCategory.length;
- 
-           // Contar cuántos productos de esta categoría están en la lista de compras
-           const purchasedProducts = productsInCategory.filter((product) => 
-             shoppingList.some((shoppingItem) => shoppingItem._id === product._id)
-           ).length;
-           
-          return(<li key={category}>
-            <div className="flex justify-between items-center">
-            <div className="flex flex-row cursor-pointer" style={{ userSelect: 'none' }}>
-            <h3 className="font-bold category-header text-xs  " >{category}</h3>
-                </div>
-              
-             
+        <>
+          <div className="mb-6">
+            <div className="flex flex-row justify-between items-center mb-4">
+              <div className="flex flex-col">
+                <p
+                  className="text-xs font-bold text-gray-300 "
+                  style={{ color: "C4C4C4" }}
+                >
+                  LISTA DEL SUPER
+                </p>
+                {shoppingList.length > 0 ?(
+                <div className="flex items-center gap-2">
+                  <p
+                    className="text-md font-bold text-gray-500 "
+                    style={{ color: "C4C4C4" }}
+                  >
+                    ESTIMADO :
+                  </p>{" "}
+                  €
+                  {shoppingList
+                    .reduce(
+                      (acc, product) =>
+                        acc +
+                        (product.price || 0) *
+                          (product.amount ? Number(product.amount) : 1),
+                      0
+                    )
+                    .toFixed(2)}
+                </div>):(<label className="text-sm text-black">No hay productos en la lista</label>)}
+              </div>
+              <div className="flex gap-3">
+                <Image
+                  onClick={() => handleSaveHistory()}
+                  className="cursor-pointer"
+                  height={20}
+                  width={20}
+                  src={Save}
+                  alt="Guardar"
+                />
+                <Image
+                  onClick={handleClearShoppingList}
+                  className="cursor-pointer"
+                  src={Bomb}
+                  alt="vaciar"
+                  width={20}
+                  height={20}
+                />
+              </div>
             </div>
 
-              <ul className="flex flex-col product-list gap-2">
-                {restCategoriesShopp[category]?.map((product) => (
-                  <ProductCardDetail product={product} key={product._id} isShoppingList={true} />
-                ))}
-              </ul>
-            
-          </li>)
-          }
-        )}
-          {/* {shoppingList?.length > 0 ?
+            <ul className="space-y-2">
+              {Object.keys(restCategoriesShopp).map((category) => {
+                const productsInCategory = restCategoriesShopp[category];
+                const totalProducts = productsInCategory.length;
+
+                // Contar cuántos productos de esta categoría están en la lista de compras
+                const purchasedProducts = productsInCategory.filter((product) =>
+                  shoppingList.some(
+                    (shoppingItem) => shoppingItem._id === product._id
+                  )
+                ).length;
+
+                return (
+                  <li key={category}>
+                    <div className="flex justify-between items-center">
+                      <div
+                        className="flex flex-row cursor-pointer"
+                        style={{ userSelect: "none" }}
+                      >
+                        <h3 className="font-bold category-header text-xs  ">
+                          {category}
+                        </h3>
+                      </div>
+                    </div>
+
+                    <ul className="flex flex-col product-list gap-2">
+                      {restCategoriesShopp[category]?.map((product) => (
+                        <ProductCardDetail
+                          product={product}
+                          key={product._id}
+                          isShoppingList={true}
+                        />
+                      ))}
+                    </ul>
+                  </li>
+                );
+              })}
+              {/* {shoppingList?.length > 0 ?
             (shoppingList.map((product:ShoppingListProduct) => (
               <ProductCardDetail  isShoppingList={true} product={product} key={product._id} />
             ))):(<label className="text-sm text-black">No hay productos en la lista</label>)} */}
-        </ul>
-      </div>
+            </ul>
+          </div>
 
-      <div>
-         <div className="flex flex-row gap-3 items-center mb-4">
-         <p className="text-xs font-bold text-gray-300 " style={{color:"C4C4C4"}}>
-         LISTA DE PRODUCTOS
-        </p>
-        <Link href="/super/add">
-          <button className="px-2 font-extrabold bg-blue-500 text-xs text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300" style={{backgroundColor:"#303030"}}>
-          +  
-          </button>
-        </Link>
-
-
-        </div>
-        <ul className="space-y-2 mb-16">
-        {Object.keys(restCategories).map((category) => {
-           const productsInCategory = restCategories[category];
-           const totalProducts = productsInCategory.length;
- 
-           // Contar cuántos productos de esta categoría están en la lista de compras
-           const purchasedProducts = productsInCategory.filter((product) => 
-             shoppingList.some((shoppingItem) => shoppingItem._id === product._id)
-           ).length;
-           
-          return(<li key={category}>
-            <div className="flex justify-between items-center">
-            <div className="flex flex-row cursor-pointer mb-2" onClick={() => toggleCategory(category)} style={{ userSelect: 'none' }}>
-            <h3 className="font-bold category-header " >{category}</h3> - ({purchasedProducts}/{totalProducts})
-                </div>
-              
-             
+          <div>
+            <div className="flex flex-row gap-3 items-center mb-4">
+              <p
+                className="text-xs font-bold text-gray-300 "
+                style={{ color: "C4C4C4" }}
+              >
+                LISTA DE PRODUCTOS
+              </p>
+              <Link href="/super/add">
+                <button
+                  className="px-2 font-extrabold bg-blue-500 text-xs text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300"
+                  style={{ backgroundColor: "#303030" }}
+                >
+                  +
+                </button>
+              </Link>
             </div>
-            {openCategories[category] && (
-              <ul className="flex flex-col product-list gap-2">
-                {restCategories[category].map((product) => (
-                  <ProductCardDetail product={product} key={product._id} />
-                ))}
-              </ul>
-            )}
-          </li>)
-          }
-        )}
-        {Otros && (
-          <li key="Otros">
-            <div className="flex justify-between items-center">
-              <h3 className="font-bold category-header cursor-pointer"  onClick={() => toggleCategory('Otros')}>Otros</h3>
-            </div>
-            {openCategories['Otros'] && (
-              <ul className="flex flex-col product-list gap-2">
-                {Otros.map((product) => (
-                  <ProductCardDetail product={product} key={product._id} />
-                ))}
-              </ul>
-            )}
-          </li>
-        )}
-      </ul>
-      </div>
-      </>
+            <ul className="space-y-2 mb-16">
+              {Object.keys(restCategories).map((category) => {
+                const productsInCategory = restCategories[category];
+                const totalProducts = productsInCategory.length;
+
+                // Contar cuántos productos de esta categoría están en la lista de compras
+                const purchasedProducts = productsInCategory.filter((product) =>
+                  shoppingList.some(
+                    (shoppingItem) => shoppingItem._id === product._id
+                  )
+                ).length;
+
+                return (
+                  <li key={category}>
+                    <div className="flex justify-between items-center">
+                      <div
+                        className="flex flex-row cursor-pointer mb-2"
+                        onClick={() => toggleCategory(category)}
+                        style={{ userSelect: "none" }}
+                      >
+                        <h3 className="font-bold category-header ">
+                          {category}
+                        </h3>{" "}
+                        - ({purchasedProducts}/{totalProducts})
+                      </div>
+                    </div>
+                    {openCategories[category] && (
+                      <ul className="flex flex-col product-list gap-2">
+                        {restCategories[category].map((product) => (
+                          <ProductCardDetail
+                            product={product}
+                            key={product._id}
+                          />
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                );
+              })}
+              {Otros && (
+                <li key="Otros">
+                  <div className="flex justify-between items-center">
+                    <h3
+                      className="font-bold category-header cursor-pointer"
+                      onClick={() => toggleCategory("Otros")}
+                    >
+                      Otros
+                    </h3>
+                  </div>
+                  {openCategories["Otros"] && (
+                    <ul className="flex flex-col product-list gap-2">
+                      {Otros.map((product) => (
+                        <ProductCardDetail
+                          product={product}
+                          key={product._id}
+                        />
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              )}
+            </ul>
+          </div>
+        </>
       )}
     </div>
   );
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,13 @@
-'use client'
-import { ThemeProvider } from "next-themes"
+"use client";
+import { ThemeProvider } from "next-themes";
+import { PrimeReactProvider } from "primereact/api";
 
-export function Providers({children}:{children:React.ReactNode}){
-    return <ThemeProvider attribute="class" defaultTheme='system' enableSystem>{children}</ThemeProvider>
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PrimeReactProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+    </PrimeReactProvider>
+  );
 }
