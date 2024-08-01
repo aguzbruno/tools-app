c0376401f55d3bf2ebf5beb568b8aef61651c548
agustin-bruno-conexa <76462423+aguzbruno@users.noreply.github.com>
2024-08-01 16:25:29 -0300

    recetas

--- app/components/Recipes/RecipeList.tsx
+++ app/components/Recipes/RecipeList.tsx
@@ -22,11 +22,42 @@ const RecipeList = () => {
   
       fetchRecipes();
     }, []);
+    const recetasCustom = [
+      "Omelette con ensalada",
+      "Ensalada pollo fideos y huevo",
+      "Ensalada pollo arroz y huevo",
+      "Ensalada pollo lenteja huevo",
+      "Pollo y papas",
+      "Pollo hongos y arroz",
+      "Pollo y ensalada",
+      "Mila con puré",
+      "Mila con papas al horno",
+      "Mila con ensalada",
+      "Mila napolitana",
+      "Tarta",
+      "Tortilla con ensalada",
+      "Wok",
+      "Albóndigas con puré y salsa",
+      "Pastas",
+      "Cerdo con ensalada/papas",
+      "Tacos",
+      "Pastel de pollo",
+      "Pollo al champiñón",
+      "Burguer",
+    ];
+    
   return (
     <div className={`container mx-auto px-4 py-6 bg-white`}>
-      <h1 className="text-3xl font-bold mb-4 text-gray-900 text-gray-100">Recetas</h1>
+      <h1 className="text-3xl font-bold mb-4 text-gray-900  text-gray-100">Recetas</h1>
+      <div className='flex flex-col gap-2'>
+      {recetasCustom.map((receta) => (
+        <div key={receta} className="p-2 bg-gray-100 bg-gray-200 rounded-md shadow-md">
+          {receta}
+        </div>
+      ))}
+      </div>
       <Link href="/recipes/add">
-        <div className="inline-block mb-6 px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 transition duration-300">
+        <div className="mt-5 inline-block mb-6 px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 transition duration-300">
           Agregar Receta
         </div>
       </Link>
